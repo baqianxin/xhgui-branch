43359b73bc691ec9eba08b299cd8eb90a3f09030
baqianxin <baqianxin@163.com>
2020-10-30 18:30:33 +0800

    update

--- external/header.php
+++ external/header.php
@@ -71,12 +71,11 @@ if (file_exists($dir . '/config/config.php')) {
     Xhgui_Config::load($dir . '/config/config.php');
 }
 unset($dir);
-if(Xhgui_Config::read('debug'))
-{
-    ini_set('display_errors',1);
+if (Xhgui_Config::read('debug')) {
+    ini_set('display_errors', 1);
 }
 $filterPath = Xhgui_Config::read('profiler.filter_path');
-if(is_array($filterPath)&&in_array($_SERVER['DOCUMENT_ROOT'],$filterPath)){
+if (is_array($filterPath) && in_array($_SERVER['DOCUMENT_ROOT'], $filterPath)) {
     return;
 }
 
@@ -101,13 +100,13 @@ if ($extension == 'uprofiler' && extension_loaded('uprofiler')) {
 } else if ($extension == 'tideways' && extension_loaded('tideways')) {
     tideways_enable(TIDEWAYS_FLAGS_CPU | TIDEWAYS_FLAGS_MEMORY);
     tideways_span_create('sql');
-} else if(function_exists('xhprof_enable')){
+} else if (function_exists('xhprof_enable')) {
     if (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION > 4) {
         xhprof_enable(XHPROF_FLAGS_CPU | XHPROF_FLAGS_MEMORY | XHPROF_FLAGS_NO_BUILTINS);
     } else {
         xhprof_enable(XHPROF_FLAGS_CPU | XHPROF_FLAGS_MEMORY);
     }
-}else{
+} else {
     throw new Exception("Please check the extension name in config/config.default.php \r\n,you can use the 'php -m' command.", 1);
 }
 
@@ -116,9 +115,7 @@ function shutdown()
     // This is our shutdown function, in
     // here we can do any last operations
     // before the script is complete.
-    header("XHProf:XXXXXXXX");
-    $http_response_header = ['"XHProf:XXXXXXXX"'];
-    header('X-Powered-By: PHP/4.4.0');
+    header('X-Powered-By: oom-php7.3');
     echo 'Script executed with success', PHP_EOL;
 }
 
@@ -135,11 +132,11 @@ register_shutdown_function(
             $data['profile'] = tideways_disable();
             $sqlData = tideways_get_spans();
             $data['sql'] = array();
-            if(isset($sqlData[1])){
-                foreach($sqlData as $val){
-                    if(isset($val['n'])&&$val['n'] === 'sql'&&isset($val['a'])&&isset($val['a']['sql'])){
-                        $_time_tmp = (isset($val['b'][0])&&isset($val['e'][0]))?($val['e'][0]-$val['b'][0]):0;
-                        if(!empty($val['a']['sql'])){
+            if (isset($sqlData[1])) {
+                foreach ($sqlData as $val) {
+                    if (isset($val['n']) && $val['n'] === 'sql' && isset($val['a']) && isset($val['a']['sql'])) {
+                        $_time_tmp = (isset($val['b'][0]) && isset($val['e'][0])) ? ($val['e'][0] - $val['b'][0]) : 0;
+                        if (!empty($val['a']['sql'])) {
                             $data['sql'][] = [
                                 'time' => $_time_tmp,
                                 'sql' => $val['a']['sql']
@@ -202,8 +199,9 @@ register_shutdown_function(
             $config = Xhgui_Config::all();
             $config += array('db.options' => array());
             $saver = Xhgui_Saver::factory($config);
-            $data['request_id']= $_SERVER['HTTP_REQUEST_ID'];
+            $data['request_id'] = $_SERVER['HTTP_REQUEST_ID'];
             $saver->save($data);
+            header('X-xxxx: oom-php7.3');
 
         } catch (Exception $e) {
             error_log('xhgui - ' . $e->getMessage());
